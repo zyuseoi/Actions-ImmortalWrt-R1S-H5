5df6ca0ab5ddee3f7b49e5dc9147d572bab4e861
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2025-11-27 18:08:35 +0800

    Update diy-part1.sh

--- SCRIPTS/R1S-H5/diy-part1.sh
+++ SCRIPTS/R1S-H5/diy-part1.sh
@@ -10,9 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#git clone https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
+Add a feed source
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default

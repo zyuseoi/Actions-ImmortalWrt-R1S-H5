c1711a65f5deba86610b65b7d1b76cae36b13571
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2026-01-21 21:38:06 +0800

    Update diy-part1.sh

--- SCRIPTS/R1S-H5/diy-part1.sh
+++ SCRIPTS/R1S-H5/diy-part1.sh
@@ -11,5 +11,6 @@
 #
 
 # Add a feed source
+sed -i '1i src-git speedtestweb https://github.com/ZeaKyX/luci-app-speedtest-web.git' feeds.conf.default
 # sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default

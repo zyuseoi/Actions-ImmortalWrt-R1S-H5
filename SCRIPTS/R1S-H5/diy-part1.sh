1be2c80e719a3878968cfc0cf81473756beedab9
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2026-02-02 16:19:28 +0800

    Update diy-part1.sh

--- SCRIPTS/R1S-H5/diy-part1.sh
+++ SCRIPTS/R1S-H5/diy-part1.sh
@@ -1,2 +1,6 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
+
+# Add a feed source
+sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages' feeds.conf.default
+sed -i '2i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall' feeds.conf.default

ad60b826e0dc3056e4b096e8e70145761770f8e5
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2026-02-02 19:39:09 +0800

    Update diy-part1.sh

--- SCRIPTS/R1S-H5/diy-part1.sh
+++ SCRIPTS/R1S-H5/diy-part1.sh
@@ -1,5 +1,2 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-
-# Add a feed source
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
